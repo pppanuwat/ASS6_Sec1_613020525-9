0f7f8b42d02257d8d55aa671654af556812fcf90
pppanuwat <panuwat.pp@kkumail.com>
2020-09-13 17:03:20 +0700

    Ass6_Sec1
    613020525-9

---
+++ AssLab6MenuFragment/app/src/main/java/com/example/asslab6menufragment/ThreeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.asslab6menufragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_three.view.*
+
+
+class ThreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view: View = inflater.inflate(R.layout.fragment_three, container, false)
+        view.female.setOnClickListener(){
+            var fragment = OneFragment()
+            replaceFragment(fragment)
+        }
+        return view
+    }
+    fun replaceFragment(female: Fragment){
+        var transaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.fragmentLayout, female)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
